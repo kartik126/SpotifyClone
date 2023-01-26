01d307e004d268a89677d1b449f609496fb3930f
kartiksharma2609 <kartik_sharma@webixun.com>
2023-01-26 18:41:13 +0530

    app authentication

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,8 @@
-import spotifyApi, { LOGIN_URL } from "lib/spotify";
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import spotifyApi, { LOGIN_URL } from "lib/spotify";
+
+// import { refreshAccessToken } from "spotify-web-api-node/src/server-methods";
 
 async function refreshAccessToken(token) {
   try {
@@ -8,15 +10,15 @@ async function refreshAccessToken(token) {
     spotifyApi.setRefreshToken(token.refreshToken);
 
     const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    console.log("REFRESHED TOKEN IS: ", refreshedToken);
     return {
       ...token,
       accessToken: refreshedToken.access_token,
-      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000, // = 1 hour as 3600 returns from spotify API
       refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
-
     };
   } catch (error) {
-    console.error(error);
+    console.error("Ha ocurrido un error, token no puedo refrescarse: " + error);
 
     return {
       ...token,
@@ -24,7 +26,8 @@ async function refreshAccessToken(token) {
     };
   }
 }
-export const authOptions = {
+
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
@@ -40,33 +43,36 @@ export const authOptions = {
   },
   callbacks: {
     async jwt({ token, account, user }) {
-      //initial signin
+      // initial signin
       if (account && user) {
         return {
           ...token,
           accessToken: account.access_token,
           refreshToken: account.refresh_token,
           username: account.providerAccountId,
-          accessTokenExpires: account.expires_at * 1000,
+          accessTokenExpires: account.expires_at * 1000, // we rae handling expiry ties in miliseconds hence * 1000
         };
       }
-      //retunr previous  token if the access token has not expired yet
+      // refrsh token
+
+      // Return previous token if the access token has not expired yet
       if (Date.now() < token.accessTokenExpires) {
         console.log("EXISTING ACCESS TOKEN IS VALID");
         return token;
       }
-      // access token has expired,so we need to get new acess token
-      console.log("ACCESS TOKEN HAS EXPIRED,REFRESHING...");
+
+      // Access token has expired, so we need to refresh it...
+      console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...");
       return await refreshAccessToken(token);
     },
 
-    async session({session,token }){
-      session.user.accessToken=token.accessToken;
-      session.user.refreshToken=token.refreshToken;
-      session.user.username=token.username;
-      return session;
-    }
-  },
-};
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
 
-export default NextAuth(authOptions);
+      return session;
+    },
+  },
+});
+// 02:12:42
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,29 +1,28 @@
-import React from "react";
 import { getProviders, signIn } from "next-auth/react";
-import spotifyImg from "../../public/pngegg.png";
-import Image from "next/image";
-function login({ providers }) {
+
+const Login = ({ providers }) => {
   return (
-    <div className="flex flex-col items-center bg-black min justify-center min-h-screen w-full">
-      <Image className="w-52 mb-5" src={spotifyImg} alt="" />
-      {Object.values(providers).map((val) => (
-        <div key={val.name}>
+    <div className="flex flex-col items-center min-h-screen w-full justify-center bg-gray-800">
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
+      {Object.values(providers).map(provider => (
+        <div key={provider.name}>
           <button
-            className="bg-[#18D860] text-white p-5 rounded-full"
-            onClick={() => signIn(val.id, { callbackUrl: "/" })}
+            className="bg-[#18D860] text-white p-4 rounded-full"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
           >
-            Login with {val.name}
+            Login with {provider.name}
           </button>
         </div>
       ))}
     </div>
   );
-}
+};
 
-export default login;
+export default Login;
 
 export async function getServerSideProps() {
   const providers = await getProviders();
+
   return {
     props: {
       providers,
